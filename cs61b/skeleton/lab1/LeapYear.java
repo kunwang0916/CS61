845626eb8b1c5ad68836db374912621382bd4fad
kun wang <kunwangus@gmail.com>
2019-09-13 23:35:16 -0700

    finished the lab1, LeapYear.java

--- cs61b/skeleton/lab1/LeapYear.java
+++ cs61b/skeleton/lab1/LeapYear.java
@@ -1,8 +1,20 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author kw 
  */
 public class LeapYear {
+    /**
+     * A leap year is either:
+     * - divisible by 400 or
+     * - divisible by 4 and not by 100.
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
 
+        return ((year % 4) == 0 && (year % 100) != 0);
+    }
+    
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
